1f9cd063e7245447a449c9239e3fdab129f67118
RaulJimenezOrtega <hhkaos@gmail.com>
2014-11-18 22:34:36 +0100

    Removing suit

--- iniciador/settings.py
+++ iniciador/settings.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = ('*',)
 # Application definition
 
 INSTALLED_APPS = (
-    'suit',
+    #'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
