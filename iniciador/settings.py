e108b9dcc622962225fbafbd04b945840f62d1ed
Raul Jimenez <hhkaos@gmail.com>
2014-12-11 19:40:50 +0000

    Done!

--- iniciador/settings.py
+++ iniciador/settings.py
@@ -18,40 +18,15 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 LOGIN_REDIRECT_URL = '/members/profile/'
 LOGOUT_URL = '/'
 
-#'''Heroku conf'''
-# Parse database configuration from $DATABASE_URL
-#import dj_database_url
 try:
     from local_settings import *
 except ImportError as e:
     pass
 
-#DATABASES = {}
-#DATABASES['default'] =  dj_database_url.config()
-
 #EMAIL ADDRESS
 FROM_EMAIL = "gerencia@iniciador.com"
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'iniciador_db',
-        'USER': 'reiniciador',
-        'PASSWORD': 'yoemprendo',
-        'HOST': '',
-        'PORT': '5432',
-    }
-}
 SERVER = 'himan.webfactional.com'
-
-'''
-    #SERVER = 'iniciador.herokuapp.com'
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#'''END Heroku conf'''
-
-ALLOWED_HOSTS = ('himan.webfactional.com',)
+ALLOWED_HOSTS = ('reiniciador.com',)
 
 
 # Application definition
@@ -67,7 +42,6 @@ INSTALLED_APPS = (
     'members',
     'djrill',
     'rest_framework',
-    #'storages',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -91,26 +65,6 @@ TIME_ZONE = 'Europe/Madrid'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
-'''
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-'''
-
-#Amazon S3
-'''
-DEFAULT_FILE_STORAGE = 'libs.storages.S3Storage.S3Storage'
-AWS_ACCESS_KEY_ID =
-AWS_SECRET_ACCESS_KEY =
-AWS_STORAGE_BUCKET_NAME =
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-'''
-
-
-# New heroku
-'''STATIC_ROOT = '/home/himan/webapps/static_media/'
-STATIC_URL = '/static/'''
-
 
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 
