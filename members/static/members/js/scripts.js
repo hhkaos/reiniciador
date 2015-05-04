37577d0b0d796b0ea27c88ac4148dab05d961473
RaulJimenezOrtega <hhkaos@gmail.com>
2015-05-04 17:04:55 +0200

    Adding modal to community listing

--- members/static/members/js/scripts.js
+++ members/static/members/js/scripts.js
@@ -39,4 +39,26 @@ $(document).ready(function(){
     console.log($t.data("group")+ ":"+len);
     $t.text($t.text()+" ("+len+")");
   });
+
+  
+  $('#myModal').modal({
+    show:false
+  });
+  $("#community tbody tr").click(function(e){
+    var $t = $(this);
+    //debugger
+    $(".modal-title").text($t.find(".first_name").text() + " " + $t.find(".last_name").text());
+    $(".modal .status").text($t.find(".status").text());
+    $(".modal .last_activity").text($t.find(".last_activity").text());
+    $(".modal .email").text($t.find(".email").text());
+    $(".modal .phone").text($t.find(".phone").text());
+    $(".modal .secondary_emails").text($t.find(".secondary_emails").text());
+    $(".modal .bio").text($t.find(".bio").text());
+    $(".modal .role").text($t.find(".role").text());
+    $(".modal .profiles").html($t.find(".profiles").html());
+    $(".modal .photo").attr("src", $t.find(".img").data("src"));
+    $(".modal .groups").text($t.find(".groups").text());
+    $('#myModal').modal('show');
+  });
+  
 });
